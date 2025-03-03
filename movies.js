f39786b6e64307969663f7aa34417209d73918af
TacoTuesday365 <soehling@gmail.com>
2025-03-03 16:15:08 -0500

    Create movies.js

---
+++ movies.js
@@ -0,0 +1,30 @@
+const aiMoviesByGeneration = {
+  "Boomers": [
+    "2001: A Space Odyssey",
+    "Westworld (1973)"
+  ],
+  "Gen X": [
+    "The Terminator",
+    "WarGames",
+    "RoboCop",
+    "Short Circuit"
+  ],
+  "Millennials": [
+    "The Matrix",
+    "A.I. Artificial Intelligence",
+    "I, Robot",
+    "Eagle Eye",
+    "Her"
+  ],
+  "Gen Z": [
+    "Ex Machina",
+    "Upgrade",
+    "Uncanny",
+    "Archive"
+  ],
+  "Gen Alpha": [
+    "M3GAN"
+  ]
+};
+
+const apiKey = "YOUR_OMDB_API_KEY"; // Replace with your actual OMDB API key!
